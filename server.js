5d45f081cc3a1362096d31b85379259c6dd211d5
Final Bosss <68625695+tony-dark-tony@users.noreply.github.com>
2021-08-21 20:17:20 +0700

    First commit

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-{\rtf1}
+const express = require('express');
+
+const app = express();
+var http = require('http').createServer(app);
+
+app.use(express.static('public'));
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+http.listen("1212");
